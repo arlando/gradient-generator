c4dc3d4848b8308354229ead8eb3f82a93a96558
arlando <arlando@gmail.com>
2016-05-27 10:04:15 -0400

    minify

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -17,6 +17,12 @@ module.exports = {
             'process.env': {
                 'NODE_ENV': JSON.stringify('production')
             }
+        }),
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                screw_ie8: true,
+                warnings: false
+            }
         })
     ],
     module: {
